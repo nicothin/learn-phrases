1257a13548eca7773838a34b7467522276526d40
Nikolay Gromov <nicothin@gmail.com>
2024-08-18 17:14:30 +0300

    remove isTabActive from StateContext

--- src/contexts/StateContext.tsx
+++ src/contexts/StateContext.tsx
@@ -1,16 +1,6 @@
-import {
-  createContext,
-  useState,
-  useMemo,
-  useEffect,
-  Dispatch,
-  SetStateAction,
-  ReactNode,
-  FC,
-} from 'react';
+import { createContext, useState, useMemo, Dispatch, SetStateAction, ReactNode, FC } from 'react';
 
 type StateContextType = {
-  isTabActive: boolean;
   isPhraseEditModalOpen: boolean;
   setIsPhraseEditModalOpen: Dispatch<SetStateAction<boolean>>;
 };
@@ -18,33 +8,14 @@ type StateContextType = {
 const StateContext = createContext<StateContextType | null>(null);
 
 export const StateContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const [isTabActive, setIsTabActive] = useState(true);
   const [isPhraseEditModalOpen, setIsPhraseEditModalOpen] = useState(false);
-
-  useEffect(() => {
-    const onFocus = () => {
-      setIsTabActive(true);
-    };
-    const onBlur = () => {
-      setIsTabActive(false);
-    };
-
-    window.addEventListener('focus', onFocus);
-    window.addEventListener('blur', onBlur);
-
-    return () => {
-      window.removeEventListener('focus', onFocus);
-      window.removeEventListener('blur', onBlur);
-    };
-  }, []);
 
   const contextValue = useMemo(
     () => ({
-      isTabActive,
       isPhraseEditModalOpen,
       setIsPhraseEditModalOpen,
     }),
-    [isTabActive, isPhraseEditModalOpen],
+    [isPhraseEditModalOpen],
   );
 
   return <StateContext.Provider value={contextValue}>{children}</StateContext.Provider>;
