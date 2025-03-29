8174454afb73f2ada856a9e0267809a26bfd18d5
Nikolay Gromov <nicothin@gmail.com>
2025-03-29 11:09:15 +0300

    add

--- src/constants.ts
+++ src/constants.ts
@@ -16,3 +16,23 @@ export const REPLACEMENTS: Replacement[] = [
   { search: /~~(.*?)~~/g, replace: '<del>$1</del>' },
   { search: /__(.*?)__/g, replace: '<span style="text-decoration:underline">$1</span>' },
 ];
+
+// export const TAGS_JSON_STRING: string = `[
+//   { "value": "Positive", "color": "green" },
+//   { "value": "Question", "color": "orange" },
+//   { "value": "Negative", "color": "red" },
+//   { "value": "Present Simple", "color": "blue" },
+//   { "value": "Present Continuous", "color": "blue" },
+//   { "value": "Present Perfect", "color": "blue" },
+//   { "value": "Past Simple", "color": "cyan" },
+//   { "value": "Future Simple", "color": "lime" },
+//   { "value": "Past Continuous", "color": "cyan" },
+//   { "value": "Present Perfect Continuous", "color": "blue" },
+//   { "value": "Future Continuous", "color": "lime" },
+//   { "value": "Past Perfect", "color": "cyan" },
+//   { "value": "Future Perfect", "color": "lime" },
+//   { "value": "Past Perfect Continuous", "color": "cyan" },
+//   { "value": "Future Perfect Continuous", "color": "lime" },
+//   { "value": "Passive voice", "color": "magenta" },
+//   { "value": "Irregular verbs", "color": "magenta" }
+// ]`;
